e690fe153942c0e5b398f587356582c48198b460
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-24 02:51:01 +0300

    Update Assignment-6.cpp

--- Mission 1/Session 1/Assignment-6.cpp
+++ Mission 1/Session 1/Assignment-6.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main() {
 
-	cout << "\n\nGuess the " << "output\n---\n";
+    cout << "\n\nGuess the " << "output\n---\n";
     cout << 10 + 20 - 5 << endl;
-	cout << "14 / 2" << "\n";
+    cout << "14 / 2" << "\n";
     cout << 17 - 10 << "\n";
     cout << "endl";
-	cout << "\nPractice makes perfect";
-	//cout << "The way to get started is to quit talking and begin doing";
+    cout << "\nPractice makes perfect";
+    //cout << "The way to get started is to quit talking and begin doing";
     
-	return 0;
+    return 0;
     cout << "\n\nBye\n\n";
 	
 }
