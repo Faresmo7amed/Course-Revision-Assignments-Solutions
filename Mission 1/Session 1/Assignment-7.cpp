632e6b5e65c523caf9f3941cb4500ef9e95c6aff
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-24 02:58:09 +0300

    Update Assignment-7.cpp

--- Mission 1/Session 1/Assignment-7.cpp
+++ Mission 1/Session 1/Assignment-7.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main() {
-    
+
+    cout << "    *****************" << endl;
     cout << "   *******************" << endl;
     cout << "  *********************" << endl;
     cout << " ***********************" << endl;
