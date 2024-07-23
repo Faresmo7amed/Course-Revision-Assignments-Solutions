a303dc1b09a503f4646a96a4df5b62ef9794443c
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-24 02:42:48 +0300

    Update Assignment-7.cpp

--- Mission 1/Session 1/Assignment-7.cpp
+++ Mission 1/Session 1/Assignment-7.cpp
@@ -4,18 +4,19 @@ using namespace std;
 int main() {
 
 	cout << "	         *" << endl;
-	cout << "		    ***" << endl;
-	cout << "		   *****" << endl;
-	cout << "		  *******" << endl;
-	cout << "		 *********" << endl;
-	cout << "	    ***********" << endl;
-	cout << "	   *************" << endl;
-	cout << "	  ***************" << endl;
-	cout << "	 *****************" << endl;
+	cout << "	        ***" << endl;
+	cout << "          *****" << endl;
+	cout << "	      *******" << endl;
+	cout << "        *********" << endl;
+	cout << "       ***********" << endl;
+	cout << "      *************" << endl;
+	cout << "     ***************" << endl;
+	cout << "    *****************" << endl;
 	cout << "   *******************" << endl;
 	cout << "  *********************" << endl;
 	cout << " ***********************" << endl;
 	cout << "*************************" << endl;
 
 	return 0;
+	
 }
