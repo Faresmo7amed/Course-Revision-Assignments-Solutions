be41ff98e13e39bad70e98ba321acfc7fe9012ac
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-24 02:35:49 +0300

    Create Assignment-5.cpp

---
+++ Mission 1/Session 1/Assignment-5.cpp
@@ -0,0 +1,2 @@
+// What is the difference between endl and \n?
+/* the key difference between them is that endl causes a flushing of the output buffer every time it is called, whereas \n does not. */
