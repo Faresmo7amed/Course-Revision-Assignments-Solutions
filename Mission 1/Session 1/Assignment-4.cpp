9b068db4ede4598d967f5789dcb086ed84dd9d06
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-24 02:28:50 +0300

    Create Assignment-4.cpp

---
+++ Mission 1/Session 1/Assignment-4.cpp
@@ -0,0 +1,77 @@
+// #include 
+/* 
+    Name: Preprocessor Directive
+    Job: It inserts a copy of the header file directly into the .cpp file prior to compilation
+*/
+
+// <iostream>
+/*
+    Name: Header File 
+    Job: It declares objects that control reading from and writing to the standard streams
+*/
+
+// using
+/*
+    Name: Directive
+    Job: It provides access to all namespace qualifiers and the scope operator
+*/
+
+// std
+/*
+    Name: Namespace
+    Job: It contains the built-in classes and declared functions
+*/
+
+// ;
+/*
+    Name: Statement Terminator
+    Job: It lets the compiler know that it's reached the end of a command
+*/
+
+// ::
+/*
+    Name: Scope Resolution Operator
+    Job: It enables us to access variables, functions, or classes defined in different scopes
+*/ 
+
+// std::
+/*
+    Name: Standard Namespace Scope Operator
+    Job: To access and use the names defined in the standard library that are declared within this namespace in our programs
+*/
+
+// cout
+/*
+    Name: Object
+    Job: It displays output to the screen
+*/
+
+// endl
+/*
+    Name: Manipulator
+    Job: It moves the cursor to the beginning of the next line
+*/
+
+// void main() {}
+/*
+    Name: Main Function
+    Job: It serves as the starting point for program execution and does not return a value
+*/
+
+// int main() {}
+/*
+    Name: Main Function
+    Job: It serves as the starting point for program execution and does return an integer value
+*/
+
+// return 0
+/*
+    Name: Return Statement
+    Job: It returns 0 as a value after the a function is excuted and it means that the program executed successfully
+*/
+
+// return 1
+/*
+    Name: Return Statement
+    Job: It returns 1 as a value after the a function is excuted and it means that the program does not execute successfully
+*/
