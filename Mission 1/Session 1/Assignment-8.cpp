42fdd9267c48f4687d0642ba16c73762cd38b623
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-24 03:04:21 +0300

    Update Assignment-8.cpp

--- Mission 1/Session 1/Assignment-8.cpp
+++ Mission 1/Session 1/Assignment-8.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
 
-	cout << "	     *" << endl;
-	cout << "	    * *" << endl;
+	cout << "	         *" << endl;
+	cout << "	        * *" << endl;
 	cout << "          *   *" << endl;
 	cout << "         *     *" << endl;
 	cout << "        *       *" << endl;
-	cout << "	*         *" << endl;
+	cout << "	    *         *" << endl;
 	cout << "      *           *" << endl;
 	cout << "     *             *" << endl;
 	cout << "    *               *" << endl;
