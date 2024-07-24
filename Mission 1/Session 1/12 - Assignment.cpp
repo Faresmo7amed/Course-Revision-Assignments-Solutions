1d99ba9976c054e8d4881fca254684baad957ea0
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-24 03:26:11 +0300

    Update 12 - Assignment.cpp

--- Mission 1/Session 1/12 - Assignment.cpp
+++ Mission 1/Session 1/12 - Assignment.cpp
@@ -7,7 +7,7 @@ int main() {
     cout << "         1   1 " << endl;
     cout << "       1   2   1 " << endl;
     cout << "     1   3   3   1" << endl;
-    cout << " 	1   4   6   4   1 " << endl;
+    cout << "   1   4   6   4   1" << endl;
     cout << " 1   5  10   10  5   1 " << endl;
     
     return 0;
