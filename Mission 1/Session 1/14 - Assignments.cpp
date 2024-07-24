b177a60bf8b06883bba0eb91837484a831e60437
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-24 03:34:14 +0300

    Create 14 - Assignments.cpp

---
+++ Mission 1/Session 1/14 - Assignments.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout <<" 12 / 2 = 6" << endl;
+    int num1 = 12, num2 = 6;
+    cout << num1 << " / " << num2 << " = " << num1 / num2;
+
+    return 0;
+
+}
