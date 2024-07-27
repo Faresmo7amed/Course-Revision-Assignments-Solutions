ed2bc776c1c109d31a92ce13c949b3b5993e503e
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-27 15:53:30 +0300

    Add files via upload

---
+++ Mission 1/Session 2/Assignment-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    cout << char(65) << endl;
+    cout << float(423.345) << endl;
+    cout << double(423.345f) << endl;
+    cout << bool(1534) << endl;
+    cout << int(' ') << endl;
+    cout << bool(0) << endl;
+    cout << to_string(2024) << endl;
+    cout << char(72) << char(101) << char(108) << char(108) << char(111) << char(44) << char(32) << char(87) << char(111) << char(114) << char(108) << char(100) << char(33);
+
+    return 0;
+
+}
