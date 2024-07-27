b9b42b2b1b4f2216386fd2d5667958d5e97ef255
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-27 15:26:35 +0300

    Update Assignment-1.cpp

--- Mission 1/Session 2/Assignment-1.cpp
+++ Mission 1/Session 2/Assignment-1.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
 	
-	cout << 65;
-	cout << 423.3453f;
-	cout << 423.3453;
-	cout << 'A';
-	cout << true;
-	cout << "Hello, World!";
+	cout << 65 << endl;
+	cout << 423.3453f << endl;
+	cout << 423.3453 << endl;
+	cout << 'A' << endl;
+	cout << true << endl;
+	cout << "Hello, World!" << endl;
 
 	return 0;
 	
