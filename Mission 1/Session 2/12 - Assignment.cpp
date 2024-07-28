f69a23dfdc808fb57e7bf165878ea5ab99789888
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-29 02:20:54 +0300

    Add files via upload

---
+++ Mission 1/Session 2/12 - Assignment.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string full_name;
+    getline(cin, full_name);
+    cout << full_name << endl;
+
+    return 0;
+
+}
