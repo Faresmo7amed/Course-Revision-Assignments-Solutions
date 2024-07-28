e717caf9db373d5374e3248c7688af0904b6788b
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-28 20:42:42 +0300

    Add files via upload

---
+++ Mission 1/Session 2/Assignment-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "123.45678910111216" << endl;
+    cout.precision(17);
+    cout << 123.45678910111216 << endl;
+
+    return 0;
+
+}
