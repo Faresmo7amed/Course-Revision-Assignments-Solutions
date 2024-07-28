ebc39fc19a5fee92cedbed31ae6adfa9d878e0eb
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-29 02:31:51 +0300

    Add files via upload

---
+++ Mission 1/Session 2/14 - Assignment.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int num1 = 12;
+    int num2 = 2;
+
+    cout << num1 + num2 << endl;
+    cout << num1 * num2 << endl;
+    cout << num1 / num2 << endl;
+    cout << num1 - num2 << endl;
+    cout << num1 % num2 << endl;
+
+    return 0;
+
+}
