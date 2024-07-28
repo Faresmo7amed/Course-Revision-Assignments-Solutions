fb22d7b8a4aef80fbca1cded855860e548e19507
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-29 02:24:12 +0300

    Add files via upload

---
+++ Mission 1/Session 2/13 - Assignment.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string fName, sName;
+    cin >> fName >> sName;
+    cout << fName << " " << sName << endl;
+
+    return 0;
+
+}
