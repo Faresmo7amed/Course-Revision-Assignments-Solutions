5ab18818b49281b02ef14a3187199ac700fe664e
Fares Mohamed <54628329+Faresmo7amed@users.noreply.github.com>
2024-07-29 02:51:18 +0300

    Add files via upload

---
+++ Mission 1/Session 2/19 - Assignment.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float num1, num2;
+    cin >> num1 >> num2;
+    cout << int(num1) % 2 << endl;
+    cout << int(num2) % 2 << endl;
+
+    return 0;
+
+}
